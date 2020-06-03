556e00a8a1ccabeccae0999355d2e0e42536fd4c
thalesgelinger <thalesgelinger99@gmail.com>
2020-06-02 21:27:15 -0300

    feat: add basic functions

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = Router()
 
-routes.get("/", (request, response) => {
-    response.send({ message: "Hello World" })
-})
+const pointsController = new PointsController()
+const itemsController= new ItemsController()
+
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.create)
+routes.get('/points/:id', pointsController.show)
+routes.get('/points', pointsController.index)
 
 export default routes
